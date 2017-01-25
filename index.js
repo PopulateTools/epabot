9ee0ebcd7ca4536754d2f7425cc49c19df4f582e
Martín González <m@martingonzalez.net>
2017-01-25 21:59:31 +0100

    Improve the tweet generation

--- index.js
+++ index.js
@@ -1,23 +1,67 @@
 var fs = require('fs');
 var d3 = require('d3');
 
-fs.readFile('data/data_small.csv', 'utf8', function(error, data) {
+fs.readFile('data/data.csv', 'utf8', function(error, data) {
   data = d3.csvParse(data);
   
-  // String parsers
-  function getGenderArticle(arg) {
-    return arg === 'Varón' ? 'un' : 'una';
+  // String parsers  
+  function getGender(d) {
+    return d === 'Varón' ? 'un hombre' : 'una mujer';
   }
   
-  function getGender(arg) {
-    return arg === 'Varón' ? 'hombre' : 'mujer';
+  // FIXME: Calculate time frames from months
+  function monthYear(d) {
+    if (d === 1) {
+      return `${d} mes`;
+    } else {
+      return `${d} meses`;
+    }
+  }
+  
+  function getTimeSearching(d) {
+    return d !== 'NA' ? `Llevo ${monthYear(d)} buscando trabajo` : 'No busco trabajo';
+  }
+  
+  function getNationality(d) {
+    return d.NAC !== 'Española' ? ` de ${d.EXREGNA}` : '';
+  }
+  
+  function getStudies(d) {
+    if (d !== 'NA') {
+      switch (d) {
+        case 'Analfabetos':
+          return 'No sé leer';
+          break;
+        case 'Educación primaria incompleta':
+          return 'No acabé primaria';
+          break;
+        case 'Educación primaria':
+          return 'Tengo la primaria';
+          break;
+        case 'Primera etapa de educación secundaria':
+          return 'Acabé la ESO';
+          break;
+        case 'Segunda etapa de educación secundaria, orientación general':
+          return 'Acabé el bachiller';
+          break;
+        case 'Segunda etapa de educación secundaria, orientación profesional':
+          return 'Estudié un módulo';
+          break;
+        case 'Educación superior':
+          return 'Acabé la universidad';
+          break;
+      }
+    } else {
+      return '';
+    }
   }
   
   // Get a random message from the array
   function getMessage(d) {
     var strings = [
-      `Tengo ${d.EDAD.toLowerCase()} y soy ${getGenderArticle(d.SEXO)} ${getGender(d.SEXO)}`,
-      `Soy ${getGenderArticle(d.SEXO)} ${getGender(d.SEXO)}, ${d.EDAD.toLowerCase()}`
+      `Tengo ${d.EDAD.toLowerCase()} y soy ${getGender(d.SEXO)}${getNationality(d)}. ${getTimeSearching(d.DTANT)}. ${getStudies(d.NFORMA)}`,
+      `Soy ${getGender(d.SEXO)}${getNationality(d)}, y tengo ${d.EDAD.toLowerCase()}. ${getTimeSearching(d.DTANT)}`,
+      `${getTimeSearching(d.DTANT)}. Soy ${getGender(d.SEXO)}${getNationality(d)} ${d.EDAD.toLowerCase()}`
     ];
     
     return strings[Math.floor(d3.randomUniform(0, strings.length)())];
